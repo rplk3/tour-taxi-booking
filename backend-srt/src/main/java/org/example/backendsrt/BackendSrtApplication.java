ca9db016686484eb60296ecc1120e523099e60b3
ranuda-perera <ranudaperera@gmail.com>
2025-11-22 04:13:02 +0530

    Add : Initial folder structure

---
+++ backend-srt/src/main/java/org/example/backendsrt/BackendSrtApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendsrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSrtApplication.class, args);
+    }
+
+}
